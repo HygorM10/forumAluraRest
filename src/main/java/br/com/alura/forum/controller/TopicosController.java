97e48f6478bc9bc62c34abca3288abdca50e7800
Hygor Martins <53188282+HygorM10@users.noreply.github.com>
2020-10-02 22:22:56 -0300

    Implementando consulta por topico e filtro por nome curso

--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -1,24 +1,29 @@
 package br.com.alura.forum.controller;
 
-import java.util.Arrays;
 import java.util.List;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-import br.com.alura.forum.modelo.Curso;
+import br.com.alura.forum.controller.dto.TopicoDto;
 import br.com.alura.forum.modelo.Topico;
+import br.com.alura.forum.repository.TopicoRepository;
 
-@Controller
+@RestController
 public class TopicosController {
 
+	@Autowired
+	private TopicoRepository topicoRepository;
+	
 	@RequestMapping("/topicos")
-	@ResponseBody
-	public List<Topico> lista(){
-		Topico topico = new Topico("Topico 1", "Teste 1", new Curso("Spring", "Programação"));
-		Topico topico1 = new Topico("Topico 2", "Teste 2", new Curso("Spring2", "Programação2"));
-		
-		return Arrays.asList(topico, topico1);
+	public List<TopicoDto> lista(String nomeCurso){
+		List<Topico> topicos = null;
+		if(nomeCurso == null) {
+			topicos = topicoRepository.findAll();
+		}else {
+			topicos = topicoRepository.findByCurso_Nome(nomeCurso);
+		}
+		return TopicoDto.converter(topicos);
 	}
 }
