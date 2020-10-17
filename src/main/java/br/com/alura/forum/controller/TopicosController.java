2759376517fcb1dcceadcb1e1264503615e3e7f9
Hygor Martins <53188282+HygorM10@users.noreply.github.com>
2020-10-17 16:21:08 -0300

    Feature-fix and pagination/ordenable

--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -1,15 +1,18 @@
 package br.com.alura.forum.controller;
 
+import static org.springframework.data.domain.Sort.Direction.DESC;
+
 import java.net.URI;
-import java.util.List;
 import java.util.Optional;
 
 import javax.transaction.Transactional;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,9 +20,11 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponentsBuilder;
 
+import br.com.alura.forum.controller.dto.DetalhesDoTopicoDto;
 import br.com.alura.forum.controller.dto.TopicoDto;
 import br.com.alura.forum.controller.form.AtualizacaoTopicoForm;
 import br.com.alura.forum.controller.form.TopicoForm;
@@ -30,7 +35,7 @@ import br.com.alura.forum.repository.TopicoRepository;
 @RestController
 @RequestMapping("/topicos")
 public class TopicosController {
-
+	
 	@Autowired
 	private TopicoRepository topicoRepository;
 	
@@ -38,29 +43,20 @@ public class TopicosController {
 	private CursoRepository cursoRepository;
 	
 	@GetMapping
-	public List<TopicoDto> lista(String nomeCurso){
-		List<Topico> topicos = null;
-		if(nomeCurso == null) {
-			topicos = topicoRepository.findAll();
-		}else {
-			topicos = topicoRepository.findByCurso_Nome(nomeCurso);
-		}
-		return TopicoDto.converter(topicos);
-	}
-	
-	@GetMapping("/{id}")
-	public ResponseEntity<TopicoDto> detalhar(@PathVariable Long id) {
-		Optional<Topico> topico = topicoRepository.findById(id);
-		if(topico.isPresent()) {
-			return ResponseEntity.ok(new TopicoDto(topico.get()));
-		}
+	public Page<TopicoDto> lista(@RequestParam(required = false) String nomeCurso, @PageableDefault(sort = "id", direction = DESC) Pageable paginacao) {
 		
-		return ResponseEntity.notFound().build();
+		if (nomeCurso == null) {
+			Page<Topico> topicos = topicoRepository.findAll(paginacao);
+			return TopicoDto.converter(topicos);
+		} else {
+			Page<Topico> topicos = topicoRepository.findByCursoNome(nomeCurso, paginacao);
+			return TopicoDto.converter(topicos);
+		}
 	}
 	
 	@PostMapping
 	@Transactional
-	public ResponseEntity<TopicoDto> cadastrar(@RequestBody @Validated @Valid TopicoForm form, UriComponentsBuilder uriBuilder) {
+	public ResponseEntity<TopicoDto> cadastrar(@RequestBody @Valid TopicoForm form, UriComponentsBuilder uriBuilder) {
 		Topico topico = form.converter(cursoRepository);
 		topicoRepository.save(topico);
 		
@@ -68,11 +64,21 @@ public class TopicosController {
 		return ResponseEntity.created(uri).body(new TopicoDto(topico));
 	}
 	
+	@GetMapping("/{id}")
+	public ResponseEntity<DetalhesDoTopicoDto> detalhar(@PathVariable Long id) {
+		Optional<Topico> topico = topicoRepository.findById(id);
+		if (topico.isPresent()) {
+			return ResponseEntity.ok(new DetalhesDoTopicoDto(topico.get()));
+		}
+		
+		return ResponseEntity.notFound().build();
+	}
+	
 	@PutMapping("/{id}")
 	@Transactional
-	public ResponseEntity<TopicoDto> atualizar(@PathVariable Long id, @Validated @RequestBody AtualizacaoTopicoForm form){
+	public ResponseEntity<TopicoDto> atualizar(@PathVariable Long id, @RequestBody @Valid AtualizacaoTopicoForm form) {
 		Optional<Topico> optional = topicoRepository.findById(id);
-		if(optional.isPresent()) {
+		if (optional.isPresent()) {
 			Topico topico = form.atualizar(id, topicoRepository);
 			return ResponseEntity.ok(new TopicoDto(topico));
 		}
@@ -82,13 +88,21 @@ public class TopicosController {
 	
 	@DeleteMapping("/{id}")
 	@Transactional
-	public ResponseEntity<?> remover(@PathVariable Long id){
+	public ResponseEntity<?> remover(@PathVariable Long id) {
 		Optional<Topico> optional = topicoRepository.findById(id);
-		if(optional.isPresent()) {
+		if (optional.isPresent()) {
 			topicoRepository.deleteById(id);
 			return ResponseEntity.ok().build();
 		}
 		
 		return ResponseEntity.notFound().build();
 	}
+
 }
+
+
+
+
+
+
+
--- src/main/java/br/com/alura/forum/controller/form/AtualizacaoTopicoForm.java
+++ src/main/java/br/com/alura/forum/controller/form/AtualizacaoTopicoForm.java
@@ -1,21 +1,36 @@
 package br.com.alura.forum.controller.form;
 
-import javax.validation.constraints.Min;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 
+import org.hibernate.validator.constraints.Length;
+
 import br.com.alura.forum.modelo.Topico;
 import br.com.alura.forum.repository.TopicoRepository;
-import lombok.Getter;
-import lombok.Setter;
 
-@Getter
-@Setter
 public class AtualizacaoTopicoForm {
-
+	
+	@NotNull @NotEmpty @Length(min = 5)
 	private String titulo;
 	
+	@NotNull @NotEmpty @Length(min = 10)
 	private String mensagem;
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getMensagem() {
+		return mensagem;
+	}
+
+	public void setMensagem(String mensagem) {
+		this.mensagem = mensagem;
+	}
 
 	public Topico atualizar(Long id, TopicoRepository topicoRepository) {
 		Topico topico = topicoRepository.getOne(id);
@@ -25,4 +40,5 @@ public class AtualizacaoTopicoForm {
 		
 		return topico;
 	}
+	
 }
